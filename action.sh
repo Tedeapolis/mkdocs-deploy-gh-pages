8e8f47ec5ccc6ed9b8cd83911dca2d6a944259ee
Niek Schoemaker <32094562+niekschoemaker@users.noreply.github.com>
2020-09-13 23:56:52 +0200

    Update action.sh

--- action.sh
+++ action.sh
@@ -37,6 +37,8 @@ if ! git config --get user.email; then
     git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 fi
 
+apt-get install git-lfs
+
 git lfs install --local
 git remote rm origin
 git remote add origin "${remote_repo}"
